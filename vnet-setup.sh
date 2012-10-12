f9d3a3838a49925633efa8c481a0a944b74c0a8f
Luca Abeni <luca.abeni@unitn.it>
2012-10-12 10:37:32 +0200

    Switch from ifconfig to ip

--- vnet-setup.sh
+++ vnet-setup.sh
@@ -12,9 +12,9 @@ N_IF=1
 eth_setup() {
   if test $ETH1_IP = NoThanks;
    then
-    sudo /sbin/ifconfig eth$IFN up
+    sudo ip link set dev eth$IFN up
    else
-    sudo /sbin/ifconfig eth$IFN $ETH1_IP 
+    sudo ip addr add $ETH1_IP/24 dev eth$IFN 
    fi
   sudo ethtool -A eth$IFN autoneg off rx off tx off
 }
@@ -23,7 +23,7 @@ virt_lan_setup() {
 #  MYSELF=$USER
 #  sudo /usr/sbin/tunctl -u $MYSELF -b -t $1
   sudo ip link add name $1 type dummy
-  sudo /sbin/ifconfig $1 up
+  sudo ip link set dev $1 up
 }
 
 macvtap_create_n() {
@@ -33,9 +33,10 @@ macvtap_create_n() {
   for i in $1
    do
     sudo ip link add link $2 name macvtap$i type macvtap $MODE
-    sudo ip link set macvtap$i address $MAC_PREFIX$i
-    sudo /sbin/ifconfig macvtap$i up
+    sudo ip link set dev macvtap$i address $MAC_PREFIX$i
+    sudo ip link set dev macvtap$i up
 #    sudo /sbin/ifconfig macvtap$i txqueuelen $QLEN
+#    sudo ip link set dev macvtap$i txqueuelen $QLEN
   done
 }
 
@@ -43,19 +44,17 @@ bridge_create() {
   MYSELF=$USER
 #  sudo /sbin/brctl addbr $2
   sudo ip link add name $2 type bridge
-  sudo /sbin/ifconfig $2 up
+  sudo ip link set dev $2 up
   for i in $1
    do
 #    sudo /usr/sbin/tunctl -u $MYSELF -b -t tap$i
     sudo ip tuntap add dev tap$i mode tap user $MYSELF
 #    sudo /sbin/brctl addif br0 tap$i
     sudo ip link set dev tap$i master $2
-    sudo /sbin/ifconfig tap$i 0.0.0.0 promisc
+#    sudo /sbin/ifconfig tap$i 0.0.0.0 promisc
+    sudo ip link set dev tap$i promisc on
+    sudo ip link set dev tap$i up 
    done
-
-#  sudo /sbin/brctl addif br0 eth$IFN
-#  sudo /sbin/ifconfig eth$IFN 0.0.0.0 promisc
-#  sudo /sbin/ifconfig br0 192.168.1.2
 }
 
 while getopts i:zvpPbB:2I:n:V:m: opt
