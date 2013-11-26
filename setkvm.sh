f3357ae831dbc249889b3bb10b745256dde6fb26
Nicola Bonelli <nicola.bonelli@gmail.com>
2013-11-26 08:17:18 +0100

    Support the virtualisation extensions in AMD CPUs too

--- setkvm.sh
+++ setkvm.sh
@@ -1,11 +1,38 @@
 MYSELF=$USER
 LIST=$(ip link show | grep macvtap | cut -d ':' -f 1)
 
-sudo /sbin/modprobe kvm_intel
+#
+# load the proper kvm kernel module...
+#
 
+load_kvm()
+{
+    INTEL=kvm_intel
+    AMD=kvm_amd
+
+    if [ ! -z "`grep "vendor_id.*GenuineIntel" /proc/cpuinfo`" ]; then
+        echo -n "Intel CPU detected: "
+        KMODULE=$INTEL
+    elif [ ! -z  "`grep "vendor_id.*AuthenticAMD" /proc/cpuinfo`" ]; then
+        echo -n "AMD CPU detected: "
+        KMODULE=$AMD
+    else 
+        echo -n "Error: CPU type not supported! ("
+        echo "`grep -m 1 vendor_id /proc/cpuinfo`)"
+        exit 1
+    fi
+        
+    echo "loading $KMODULE..."
+    sudo /sbin/modprobe $KMODULE $*
+}
+
+load_kvm
+ 
 sudo chown $MYSELF /dev/kvm
 sudo chown $MYSELF /dev/vhost-net
 for TAP_N in $LIST
  do
   sudo chown $MYSELF /dev/tap$TAP_N
  done
+
+echo "done."
