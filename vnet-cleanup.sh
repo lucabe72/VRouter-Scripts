c363f97c428f6fdb237d96904636d56dc078234b
Luca Abeni <luca.abeni@unitn.it>
2014-01-14 11:29:10 +0100

    Rewrite vnet-cleanup.sh in a much simpler way
    Now it even works ;-)

--- vnet-cleanup.sh
+++ vnet-cleanup.sh
@@ -1,71 +1,13 @@
-#!/bin/bash
+TAPS=$(ip link show | grep tap | cut -d ':' -f 2 | cut -d '@' -f 1)
 
-get_tap_n() {
-  num=$(ip link show | grep tap | grep -v macvtap | wc -l)  
-  RES=""
-  for i in 0..$num
-   do
-    N=$(ip link show | grep tap | cut -d ':' -f 2)
-    RES="$RES $N"
-   done
-  echo $RES
-}
+echo $TAPS
 
-get_macvtap_n() {
-  num=$(ip link show | grep macvtap | wc -l)   
-  RES=""
-  for i in 0..$num
-   do
-    N=$(ip link show | grep macvtap | cut -d ':' -f 2 | cut -d '@' -f 1)
-    RES="$RES $N"
-   done
-  echo $RES
-}
-
-del_tap_n(){
-for i in $1
-  do
-     tunctl -d $i 
-  done
-}
-
-
-del_macvtap_n(){
-for i in $1
-  do
-     ip link del $i
-  done
-}
-
-del_bridge_n(){
-for i in $bridge_name
-  do
-     ifconfig $i down
-     brctl delbr $i
-  done
-}
-
-while getopts b: opt
+for D in $TAPS
  do
-  echo "Opt: $opt"
-  case "$opt" in
-    b)		bridge_name=$OPTARG;;
-    [?])	print >&2 "Usage: $0 [-b]"
-		exit 1;;
-  esac
+  sudo /home/vrouter/bin/ip link del $D
  done
 
-#if (($# != 1))
-#then
-#echo "Usage: $0 [-b]"
-#exit 1
-#fi
-
-TAP_N=$(get_tap_n);
-MACVTAP_N=$(get_macvtap_n);
-
-
-del_tap_n "$TAP_N";
-del_macvtap_n "$MACVTAP_N";
-del_bridge_n;
-
+for D in $1
+ do
+  sudo /home/vrouter/bin/ip link del $D
+ done
