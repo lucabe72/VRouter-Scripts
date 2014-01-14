39142cb9a40e82bef5e01f2beaf87b097388ea5f
Luca Abeni <luca.abeni@unitn.it>
2014-01-14 11:33:10 +0100

    Update rshd.sh to work with busybox's netcat

--- rshd.sh
+++ rshd.sh
@@ -1,1 +1,10 @@
-while true; do nc -l -p 8888 -s 192.168.1.10 -e /bin/sh; done
+#while true; do nc -l -p 8888 -s 192.168.1.10 -e /bin/sh; done
+if test "x$1" != "x"
+ then
+  S="-s $1"
+ fi
+
+while true;
+ do
+  nc -l -p 8888 $S -e /bin/sh;
+ done
